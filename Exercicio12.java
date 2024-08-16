cca25190c484a14493d98350bbc529b5d61e46a7
VininLeao <104996741+VininLeao@users.noreply.github.com>
2024-08-15 23:51:57 -0300

    Update Exercicio12.java

--- Exercicio12.java
+++ Exercicio12.java
@@ -6,39 +6,48 @@ public class Exercicio12 {
         Scanner scanner = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("#.00");
 
-        int tanquesCheios = 0, quilometrosRodados = 0, litrosUtilizados = 0, quilometrosTotal = 0, litrosTotal = 0;
-        double quilometrosLitro = 0, mediaQuilometrosLitro = 0, mediaQuilometrosRodados = 0;
+        int tanquesCheios = 0;
+        int quilometrosTotal = 0;
+        int litrosTotal = 0;
+        double quilometrosLitro;
+        double mediaQuilometrosLitro;
+        double mediaQuilometrosRodados = 0;
 
-        System.out.println("Para encerrar o programa, digite uma quantidade de quilômetros rodados negativa");
+        System.out.println("Para encerrar o programa, digite uma quantidade de quilômetros rodados negativa.");
 
-        while (quilometrosRodados >= 0) {
-            System.out.println("Digite a quantidade de quilômetros rodados no tanque " + (tanquesCheios + 1));
-            quilometrosRodados = scanner.nextInt();
+        while (true) {
+            System.out.println("Digite a quantidade de quilômetros rodados no tanque " + (tanquesCheios + 1) + ":");
+            int quilometrosRodados = scanner.nextInt();
 
             if (quilometrosRodados < 0) {
-                System.out.println("O programa foi encerrado, mostrando o resultado: \n");
+                System.out.println("O programa foi encerrado, mostrando o resultado:\n");
                 break;
             }
 
             System.out.println("Digite a quantidade de litros utilizados com o tanque cheio:");
-            litrosUtilizados = scanner.nextInt();
+            int litrosUtilizados = scanner.nextInt();
 
             quilometrosTotal += quilometrosRodados;
             litrosTotal += litrosUtilizados;
+
             quilometrosLitro = (double) quilometrosRodados / litrosUtilizados;
             tanquesCheios++;
 
-            System.out.println("\nO consumo em quilômetros por litro de combústivel no tanque " + (tanquesCheios) + " foi de: " + df.format(quilometrosLitro) + " km/l");
+            System.out.println("\nO consumo em quilômetros por litro de combustível no tanque " + tanquesCheios + " foi de: " + df.format(quilometrosLitro) + " km/l");
             System.out.println("A quantidade de quilometragem combinada até agora é de: " + quilometrosTotal + " km");
             System.out.println("A quantidade de litros de combustível utilizados até agora é de: " + litrosTotal + " litros\n");
         }
 
-        mediaQuilometrosLitro = (double) quilometrosTotal / litrosTotal;
-        mediaQuilometrosRodados = (double) quilometrosTotal / tanquesCheios;
+        if (tanquesCheios > 0) {
+            mediaQuilometrosLitro = (double) quilometrosTotal / litrosTotal;
+            mediaQuilometrosRodados = (double) quilometrosTotal / tanquesCheios;
 
-        System.out.println("Total de quilômetros combinados: " + quilometrosTotal + " km");
-        System.out.println("Total de litros utilizados: " + litrosTotal + " litros");
-        System.out.println("Média de quilômetros rodados: " + df.format(mediaQuilometrosRodados) + " km");
-        System.out.println("Média de consumo de combustivel em quilômetros por litro de todos os tanques: " + df.format(mediaQuilometrosLitro) + " km/l");
+            System.out.println("Total de quilômetros combinados: " + quilometrosTotal + " km");
+            System.out.println("Total de litros utilizados: " + litrosTotal + " litros");
+            System.out.println("Média de quilômetros rodados por tanque: " + df.format(mediaQuilometrosRodados) + " km");
+            System.out.println("Média de consumo de combustível em quilômetros por litro de todos os tanques: " + df.format(mediaQuilometrosLitro) + " km/l");
+        } else {
+            System.out.println("Nenhum dado foi inserido.");
+        }
     }
 }
